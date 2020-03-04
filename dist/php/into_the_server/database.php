bb09b0c646b8041150770b3701d3d7b824dd24d9
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-04 19:51:59 +0100

    tutto funzionante, da strutturare l'index.php

--- dist/php/into_the_server/database.php
+++ dist/php/into_the_server/database.php
@@ -3,24 +3,15 @@
   $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn && $conn->connect_error) {
       echo "Connection failed: " . $conn->connect_error;
+      die();
     }
-    // $sql = "SELECT id, room_number, floor FROM stanze";
-    // $result = $conn->query($sql);
-    // if ($result && $result->num_rows > 0) {
-    //   $rooms = [];
-    // while($row = $result->fetch_assoc()) {
-    //   $rooms[] = $row;
-    //   var_dump($row);
-    //   echo $row['id'];
-    // }
-    // var_dump($rooms);
-    $sql = "SELECT room_number, floor FROM stanze";
+    $sql = "SELECT id, room_number, floor FROM stanze";
     $result = $conn->query($sql);
     if ($result && $result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-    echo "Stanza N. ". $row['room_number']. " piano: ".$row['floor'];
-    }
+      $rooms = [];
+      while($row = $result->fetch_assoc()) {
+        $rooms[] = $row;
+      }
     } elseif ($result) {
     echo "0 results";
     } else {
