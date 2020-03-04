b1c022a474138445e234bb0c2d978521cb181062
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-04 18:04:56 +0100

    fixati include in index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php
-    include __DIR__."/dist/php/head_and_above.php";
-    include __DIR__."/dist/php/body.php";
+    include __DIR__."/dist/php/head/head.php";
+    include __DIR__."/dist/php/body/body.php";
   ?>
 
 </html>
