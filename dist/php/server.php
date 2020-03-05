45b6eeb31e5168635f5c9288253a32c99f831911
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-05 16:25:48 +0100

    aggiornata struttura directory "into_the_server"

--- dist/php/server.php
+++ dist/php/server.php
@@ -1,3 +1,3 @@
 <?php
   include __DIR__."/into_the_server/env.php";
-  include "$basePath/dist/php/into_the_server/hoteldb/hoteldb.php";
+  include "$basePath/dist/php/into_the_server/databases/db_hotel/db_hotel.php";
