8e575364e6766ea4b93ad3a2970b2ab497300802
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-05 16:27:14 +0100

    riposizionato "stanze.php"

--- dist/php/into_the_server/databases/db_hotel/db_hotel.php
+++ dist/php/into_the_server/databases/db_hotel/db_hotel.php
@@ -1,3 +1,3 @@
 <?php
   include $_SERVER['DOCUMENT_ROOT']."/mamp_public/php-hotel-crud/dist/php/into_the_server/env.php";
-  include "$basePath/dist/php/into_the_server/databases/db_hotel/entities/stanze/stanze.php";
+  include "$basePath/dist/php/into_the_server/databases/db_hotel/entities/stanze.php";
